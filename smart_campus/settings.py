673c7c102f22d13b74dd7b4b408eaed87d1983d5
DharhsniKolanchinathan <vikashini1105@gmail.com>
2026-03-16 23:17:53 +0530

    Fix ALLOWED_HOSTS

--- smart_campus/settings.py
+++ smart_campus/settings.py
@@ -27,13 +27,16 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-g#0sfwo!x2)&&^8!)51ix
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = config(
+    "ALLOWED_HOSTS",
+    default="localhost,127.0.0.1,loast-and-found.onrender.com"
+).split(",")
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

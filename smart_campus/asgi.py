a9f79814f9732cec58278ef9f0f8c6054844ac5d
DharhsniKolanchinathan <vikashini1105@gmail.com>
2026-03-17 14:35:58 +0530

    Fix ASGI Django initialization

--- smart_campus/asgi.py
+++ smart_campus/asgi.py
@@ -1,16 +1,23 @@
 import os
-from django.core.asgi import get_asgi_application
+import django
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "smart_campus.settings")
+
+django.setup()   # IMPORTANT: load Django first
+
 import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smart_campus.settings')
-
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
-        )
-    ),
-})
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AuthMiddlewareStack(
+            URLRouter(
+                chat.routing.websocket_urlpatterns
+            )
+        ),
+    }
+)
